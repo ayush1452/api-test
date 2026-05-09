27531f06ac0cf438e89e4e46f082fc519db936ab
Ayush Sharma <ayush0810sharma@gmail.com>
2026-05-10 01:11:09 +0530

    Add add-account flow

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      statusBarBrightness: Brightness.light,
+    ),
+  );
+  runApp(const SpendlyApp());
+}
+
+class SpendlyApp extends StatelessWidget {
+  const SpendlyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'UX Pilot Finance',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF1FAF8F),
+          brightness: Brightness.light,
+        ),
+        scaffoldBackgroundColor: const Color(0xFFF6F8F7),
+        textTheme: GoogleFonts.interTextTheme(Theme.of(context).textTheme),
+        fontFamily: GoogleFonts.inter().fontFamily,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
